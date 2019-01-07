43fed4ee62048d0b65275973e7d9628dbc7a8667
uninoma <uninoma@gmail.com>
2019-01-07 02:38:12 +0200

    port fix heroku

---
+++ static/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(
+    function() {
+        $("button").click(function(event) {
+            console.log(event.target.innerText)
+            displayController(event.target.innerText,textDecrease)
+        });
+    }
+);
+
+function displayController(val,callback) {
+
+    switch(val){
+        case "C":
+            $("#display .input").html("");
+            break;
+        case "⇜"://backspace
+            $("#display .input").text($("#display .input").text().slice(0,-1))
+            break;
+        case "MC":
+            console.log(val);
+            var dataToSend = JSON.stringify({"type":"calculate","value":"MC"});
+            WS.send(dataToSend);
+            break;
+        case "MR":
+            sendData("MR","")
+            break;
+        case "M+":
+            sendData("M+",$("#display .input").text())
+            break;
+        case "M-":
+            sendData("M-",$("#display .input").text())
+            break;
+        case "=":
+            sendData("calculate",$("#display .input").text())
+            break;
+        default:
+            $("#display .input").html($("#display .input").html()+val);
+
+            break;
+    }
+    callback()
+}
+
+function sendData(type,data) {
+    var dataToSend = JSON.stringify({"type":type,"data":data});
+    WS.send(dataToSend);
+}
+function textDecrease(){
+    var inputWidth=$(".input").width()
+    var displayWidth=$("#display").width();
+    console.log("input width:",inputWidth);
+    console.log("display width:",displayWidth);
+    var diffrence=inputWidth-displayWidth;
+    console.log("raznica:",diffrence)
+    if(diffrence>-40){
+        $(".input").css("font-size", $(".input").css("font-size").replace("px","") - 12);
+    }
+    if($(".input").text().length<18){
+           $(".input").css("font-size",window.getComputedStyle(document.getElementById("display"))["font-size"])
+    }
+}
